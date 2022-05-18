1ec463c7734d4a4ae64ca2c3b169743ad9c9f47e
Hooannn <hoanthui123@gmail.com>
2022-05-18 21:37:08 +0700

    update scrollbar style

--- src/App.vue
+++ src/App.vue
@@ -39,6 +39,25 @@ export default {
   --orange:rgb(255, 117, 85);
   --rose:#fb5252;
 }
+/* srollbar style */
+::-webkit-scrollbar-track
+{
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+	border-radius: 10px;
+	background-color: #F5F5F5;
+}
+
+::-webkit-scrollbar
+{
+	width: 8px;
+	background-color: #F5F5F5;
+}
+
+::-webkit-scrollbar-thumb
+{
+	background-color: var(--dark);
+}
+/* */
 .modal-backdrop {
   background-color:rgba(0,0,0,0.4);
 }
